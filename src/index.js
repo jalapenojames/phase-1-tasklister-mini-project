1d350c5544aa149b3e8491e8e04db83c2b4ff86c
James <jaames.chen@gmail.com>
2021-04-09 15:03:45 -0400

    Completed minimum req for tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // As a user, I should be able to type a task into the input field.
+  
+  // Puts what we enter into the DOM
+  document.addEventListener("submit", function(event) {
+    let inputTask = document.getElementById('new-task-description').value
+    event.preventDefault()
+    console.log(`You submitted ${inputTask}`)
+    addTask(inputTask)
+  })
+
+  // append submitted tasks to the dom
+  
 });
+
+// When we press button, the value inside is sent to the DOM
+function addTask (task) {
+  let newLi = document.createElement('li')
+  newLi.innerText = task
+  document.getElementById('tasks').appendChild(newLi)
+}
+
+
